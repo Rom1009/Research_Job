6734a3f81bdc9af8dd73a29418d840c6a819689d
Rom1009 <quynguyencurit2002@gmail.com>
2026-07-01 06:52:16 +0700

    Push

---
+++ backend/db/db.py
@@ -0,0 +1,14 @@
+from sqlmodel import create_engine, SQLModel, Session
+from backend.utils.config import settings
+from typing import Generator 
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
---
+++ backend/src/schema/model.py
@@ -0,0 +1,3 @@
+from sqlmodel import SQLModel, Field, Column, JSON
+from uuid import uuid4
+from typing import Literal, Optional
